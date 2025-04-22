18509c7555089ba3f20fe47d1b00d12cd89875a0
luyang <luyang>
2025-04-22 22:10:25 +0800

    update

--- js/math.709e7cabd87ed8a988dc621ae40e371b50aca901ad9b6afadefac3e376945193.js
+++
@@ -1,1 +0,0 @@
-(()=>{(function(){document.addEventListener("DOMContentLoaded",function(){renderMathInElement(document.body,{delimiters:[{left:"\\[",right:"\\]",display:!0},{left:"$$",right:"$$",display:!0},{left:"\\(",right:"\\)",display:!1},{left:"$",right:"$",display:!1}],throwOnError:!1})})})();})();
